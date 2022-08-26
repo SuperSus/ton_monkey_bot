6e91b23036834e947ceff4af17d16e77cf5f2821
sikorsky <alexandr.sikorsky@appodeal.com>
2022-08-26 13:36:24 +0100

    Small configuration enhancements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from StandardError do |e|
+    # e.message - sentry
+    flash[:error] = "Попробуй еще раз!"
+    redirect_back fallback_location: root_path
+  end
 end
