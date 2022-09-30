4068117ea981dfc7e11f4dd0249810180e574929
Sikorsky Alexander <sikorskyalexander08@gmail.com>
2022-09-30 17:59:17 +0300

    Fix nft deployer

--- app/services/nft/deployer_service.rb
+++ app/services/nft/deployer_service.rb
@@ -8,21 +8,34 @@ module Nft
 
     def call
       node_process.exec!
-      handle_error(node_process.err) if node_process.err.present?
-      Rails.logger.info(node_process.out)
+    ensure
+      handle_output
     end
 
     private
 
     def node_process
-      @node_process ||= POSIX::Spawn::Child.build('node', 'nft_deployer/index.js')
+      @node_process ||= POSIX::Spawn::Child.build('node', 'nft_deployer/index.js', timeout: 60)
     end
 
-    def handle_error(err)
-      return if err.blank?
+    def handle_output
+      errors = []
+      errors << node_process.out if node_process.out['[Deployer] deployNft error']
+      errors << node_process.err if node_process.err.present? && err?(node_process.err)
+      errors = errors.join(', ')
 
-      Rails.logger.error(err)
-      raise err
+      if errors.present?
+        Rails.logger.error(errors)
+        raise errors
+      end
+
+      Rails.logger.info(node_process.out) if node_process.out.present?
+    end
+
+    def err?(err)
+      return false if err['Start index ${this.deployIndex} bigger than supplied nfts amount'] # ignore already deployed err
+
+      true
     end
   end
 end
